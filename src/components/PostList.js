965ece683ce5d698dfe42b56a08f588a4893d3e7
Junjie Chen <junjie.chen18@gmail.com>
2019-06-22 14:33:17 -0400

    feat(src): create mapDispatchToProps method

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends Component {
   render() {
     return <div>Post List</div>;
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  fetchPosts() {
+    dispatch(fetchPosts());
+  }
+});
 
 export default connect(null)(PostList);
