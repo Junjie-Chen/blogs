11a354bb2d61424a0e05cfddbf5eb188584dcff8
Junjie Chen <junjie.chen18@gmail.com>
2019-06-24 17:58:40 -0400

    feat(src): create mapDispatchToProps method

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 class UserHeader extends Component {
   render() {
     return <div>User Header</div>;
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  fetchUser(id) {
+    dispatch(fetchUser(id));
+  }
+});
 
 export default connect(null)(UserHeader);
