c3ec06ee951a69fadd0f279fe32e967e6cefbfbf
Junjie Chen <junjie.chen18@gmail.com>
2019-06-24 17:55:19 -0400

    feat(src): connect UserHeader component to Provider component

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class UserHeader extends Component {
   render() {
@@ -6,4 +7,4 @@ class UserHeader extends Component {
   }
 }
 
-export default UserHeader;
+export default connect()(UserHeader);
