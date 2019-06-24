36e60df52d6e1ff146edc7240c16169f614a448f
Junjie Chen <junjie.chen18@gmail.com>
2019-06-24 18:43:42 -0400

    feat(src): add switch statement to users reducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,3 +1,8 @@
-export default () => {
-  return [];
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_USER':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
